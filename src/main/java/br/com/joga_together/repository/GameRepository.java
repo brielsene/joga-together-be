057d7fede33719a302862a2f4643303a58f78091
gabriel <sene300@gmail.com>
2026-02-11 19:05:20 -0300

    feat: ajuste + validacao game existente

--- src/main/java/br/com/joga_together/repository/GameRepository.java
+++ src/main/java/br/com/joga_together/repository/GameRepository.java
@@ -3,7 +3,9 @@ package br.com.joga_together.repository;
 import br.com.joga_together.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface GameRepository extends JpaRepository<Game, UUID> {
+    Optional<Game> findByTitle(String name);
 }
